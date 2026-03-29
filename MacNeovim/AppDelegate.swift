8b75f75f48f448f6a5787588c4923be1b5a6b8bd
Rainux Luo <rainux@gmail.com>
2026-03-30 01:33:20 +0800

    Add blank Xcode project

---
+++ MacNeovim/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MacNeovim
+//
+//  Created by Rainux Luo on 2026/3/30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MacNeovimTests/MacNeovimTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacNeovimTests.swift
+//  MacNeovimTests
+//
+//  Created by Rainux Luo on 2026/3/30.
+//
+
+import Testing
+@testable import MacNeovim
+
+struct MacNeovimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
